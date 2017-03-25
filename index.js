8878b8329038ece814f2a9a5d8bcbe34a3acb778
Sindre Sorhus <sindresorhus@gmail.com>
2017-03-26 01:50:02 +0700

    Update user agent

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict';
-module.exports = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36';
+module.exports = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36';
