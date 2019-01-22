37bbcc24ad1e3d784e39d8cbefc16f95397e6914
Vinay <vinaybedre@gmail.com>
2019-01-22 16:09:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict';
-module.exports = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3673.0 Safari/537.36';
+module.exports = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.127 Safari/537.36';
