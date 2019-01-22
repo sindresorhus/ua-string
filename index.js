be6726fe51966cd14517d12d13664ccd4f3f9e8e
Vinay <vinaybedre@gmail.com>
2019-01-22 14:51:50 +0100

    Update UA-String
    Updated user agent string with latest Chrome V73

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict';
-module.exports = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36';
+module.exports = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3673.0 Safari/537.36';
