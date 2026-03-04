a08946520c6ec223c9e311dc7a5b25fed9d73107
Sindre Sorhus <sindresorhus@gmail.com>
2026-03-04 14:54:13 +0700

    Update user-agent string
    Fixes #4

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const userAgentString = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15';
+const userAgentString = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36';
 
 export default userAgentString;
